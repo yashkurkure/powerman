5551d77d5bd7a9179c611517af35fc0c6a04607b
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-11 17:34:03 +0000

    Produce rapl reading continuous

--- rapl-readings.py
+++ rapl-readings.py
@@ -1,10 +1,8 @@
 # Python script to run rapl-read.c at intervals to gather power consumption
 import subprocess
-
+import time
 
 def read_rapl():
-    
-    
     args = ("./rapl-read", "-m")
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
@@ -12,7 +10,10 @@ def read_rapl():
 
     package1_energy = float(output[49].decode().strip().split(':')[1].split('J')[0].strip())
     package2_energy = float(output[53].decode().strip().split(':')[1].split('J')[0].strip())
+    timestamp = time.time()
+    return [timestamp, package1_energy, package2_energy]
 
-    return output
-
-readings = read_rapl()
+print(f'Time\t\t\t\t\t\tPackage1\t\t\tPackage2')
+while True:
+    reading = read_rapl()
+    print(f'{reading[0]}\t\t\t\t{reading[1]}\t\t\t{reading[2]}')
