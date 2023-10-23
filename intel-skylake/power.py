78901c7ac0d2102a04b8b8ad05497470562017c2
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-23 12:25:20 -0600

    Adjust for single package

--- intel-skylake/power.py
+++ intel-skylake/power.py
@@ -4,6 +4,7 @@ import time
 import sys
 import os
 outgraph = sys.argv[1]
+num_packages = int(sys.argv[2])
 
 start_timestamp = time.time()
 
@@ -14,9 +15,9 @@ def read_rapl():
     energy_counter1_0 = int(f.readline())
     f.close()
 
-    f = open("/sys/class/powercap/intel-rapl/intel-rapl:1/energy_uj", "r")
-    energy_counter2_0 = int(f.readline())
-    f.close()
+    # f = open("/sys/class/powercap/intel-rapl/intel-rapl:1/energy_uj", "r")
+    # energy_counter2_0 = int(f.readline())
+    # f.close()
 
     time.sleep(1)
 
@@ -25,20 +26,20 @@ def read_rapl():
     energy_counter1_1 = int(f.readline())
     f.close()
 
-    f = open("/sys/class/powercap/intel-rapl/intel-rapl:1/energy_uj", "r")
-    energy_counter2_1 = int(f.readline())
-    f.close()
+    #f = open("/sys/class/powercap/intel-rapl/intel-rapl:1/energy_uj", "r")
+    #energy_counter2_1 = int(f.readline())
+    #f.close()
 
     power_counter1 = (energy_counter1_1 - energy_counter1_0)/1000000
-    power_counter2 = (energy_counter2_1 - energy_counter2_0)/1000000
+    #power_counter2 = (energy_counter2_1 - energy_counter2_0)/1000000
 
     timestamp = time.time()
-    return [int(t0 - start_timestamp), power_counter1, power_counter2]
+    return [int(t0 - start_timestamp), power_counter1]
 
 
 x = []
 y1 = []
-y2 = []
+# y2 = []
 
 try:
     while True:
@@ -46,7 +47,7 @@ try:
         print(reading)
         x.append(reading[0])
         y1.append(reading[1])
-        y2.append(reading[2])
+        # y2.append(reading[2])
 except KeyboardInterrupt:
     print('Stoping rapl reads...')
 
@@ -54,7 +55,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 plt.plot(x, y1, color = 'green', marker = 'o', label='Package 1')
-plt.plot(x, y2, color = 'red', marker = 'x', label='Package 2')
+# plt.plot(x, y2, color = 'red', marker = 'x', label='Package 2')
 plt.ylabel('Power(J/s)')
 plt.xlabel('Sample window start time (s)')
 plt.legend(title = f'Power(J/s) vs Time')
--- intel-skylake/powercap.py
+++ intel-skylake/powercap.py
@@ -12,14 +12,14 @@ else:
 
 
 # sudo stress --cpu 8 --timeout 20
-f = open("", "w+")
-print('Setting power cap to 50W')
-f.write('50000000')
+f = open("/sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw", "w+")
+print('Setting power cap to 25W')
+f.write('25000000')
 f.close()
 
 time.sleep(10)
 
 f = open("/sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw", "w+")
-print('Setting power cap back to 125W')
-f.write('125000000')
+print('Setting power cap back to 45W')
+f.write('45000000')
 f.close()
