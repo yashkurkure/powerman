3e26d6aa00ec27b0002ecdb9b47c50d11b64d184
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-26 16:49:34 -0600

    run pbs install as sudo

--- cloudlab/openPBS/install-ubuntu-client.sh
+++ cloudlab/openPBS/install-ubuntu-client.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install openPBS on Ubuntu 20.04
-apt update
-apt-get -y install gcc make libtool libhwloc-dev libx11-dev \
+sudo apt update
+sudo apt-get -y install gcc make libtool libhwloc-dev libx11-dev \
       libxt-dev libedit-dev libical-dev ncurses-dev perl \
       postgresql-server-dev-all python-dev tcl-dev tk-dev swig \
       libexpat-dev libssl-dev libxext-dev libxft-dev autoconf \
       automake
-apt-get -y install expat libedit2 postgresql python sendmail-bin \
+sudo apt-get -y install expat libedit2 postgresql python sendmail-bin \
       sudo tcl tk libical1a
 
-apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb
+sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb
 
 
