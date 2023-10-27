d9f45332c3181bbb257863a7c97fe619ec7babcd
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-27 11:34:54 -0600

    Add config to install script

--- cloudlab/openPBS/install-ubuntu-compute.sh
+++ cloudlab/openPBS/install-ubuntu-compute.sh
@@ -7,4 +7,14 @@ sudo apt-get -y install expat libedit2 postgresql python3 postgresql-contrib sen
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb
 
+sudo echo "PBS_EXEC=/opt/pbs
+PBS_SERVER=head.openpbs-install.schedulingpower.emulab.net
+PBS_START_SERVER=0
+PBS_START_SCHED=0
+PBS_START_COMM=0
+PBS_START_MOM=1
+PBS_HOME=/var/spool/pbs
+PBS_CORE_LIMIT=unlimited
+PBS_SCP=/usr/bin/scp" > /etc/pbs.conf
 
+sudo systemctl start pbs
