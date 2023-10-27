33f79f605f009394a78b6869715c13a5d9177d4d
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-27 12:27:04 -0600

    Ansible on head and cleanup

--- cloudlab/openPBS/install-ubuntu-server.sh
+++ cloudlab/openPBS/install-ubuntu-server.sh
@@ -2,7 +2,12 @@
 
 # Install openPBS on Ubuntu 20.04
 sudo apt update
-sudo apt-get -y install expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
+sudo apt -y install expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
       sudo tcl tk libical3 postgresql-server-dev-all
 
-sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
+sudo apt -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
+
+# Install Ansible
+sudo apt -y install software-properties-common
+sudo add-apt-repository --y --update ppa:ansible/ansible
+sudo apt install ansible
