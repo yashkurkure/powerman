4ef9e9eda1a3c8acd80da1fe98e71ea9acdc3d97
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-27 14:06:04 -0600

    ansible worker node inventory creation

---
+++ cloudlab/ansible/create_worker_inventory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Usage: create_worker_inventory.sh <number of worker nodes>
+
+# Specify the number of nodes as a argument to the script
+numnodes=$1
+
+# Check if atleast 1 worker node
+if ((numnodes < 1)); then
+        echo "Number of worker nodes should be atleast 1"
+        exit 1
+fi
+
+# Generate the ansible inventory
+echo "[workernodes]"
+for ((i=0; i<numnodes; i++)); do
+            echo "$(hostname | sed "s/head/node$i/")"
+done
+echo "[workernodes:vars]"
+echo "ansible_user=root"
+echo "host_key_checking=False"
