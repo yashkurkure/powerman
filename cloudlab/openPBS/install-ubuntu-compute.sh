9deebc3c0fcb681aad9300cc2f836c478215cd56
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-26 22:12:48 -0600

    10th attempt at running server

--- cloudlab/openPBS/install-ubuntu-compute.sh
+++ cloudlab/openPBS/install-ubuntu-compute.sh
@@ -2,13 +2,8 @@
 
 # Install openPBS on Ubuntu 20.04
 sudo apt update
-sudo apt-get -y install gcc make libtool libhwloc-dev libx11-dev \
-      libxt-dev libedit-dev libical-dev ncurses-dev perl \
-      postgresql-server-dev-all python-dev tcl-dev tk-dev swig \
-      libexpat-dev libssl-dev libxext-dev libxft-dev autoconf \
-      automake
-sudo apt-get -y install expat libedit2 postgresql python sendmail-bin \
-      sudo tcl tk libical1a
+sudo apt-get -y expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
+      sudo tcl tk libical3 postgresql-server-dev-all
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb
 
--- cloudlab/openPBS/install-ubuntu-server.sh
+++ cloudlab/openPBS/install-ubuntu-server.sh
@@ -2,21 +2,16 @@
 
 # Install openPBS on Ubuntu 20.04
 sudo apt update
-sudo apt-get -y install gcc make libtool libhwloc-dev libx11-dev \
-      libxt-dev libedit-dev libical-dev ncurses-dev perl \
-      postgresql-server-dev-all python-dev tcl-dev tk-dev swig \
-      libexpat-dev libssl-dev libxext-dev libxft-dev autoconf \
-      automake
-sudo apt-get -y install expat libedit2 postgresql python sendmail-bin \
-      sudo tcl tk libical1a
+sudo apt-get -y expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
+      sudo tcl tk libical3 postgresql-server-dev-all
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
 
 # Start postgresql service
-sudo ystemctl start postgresql.service
-
+sudo systemctl start postgresql
 
 # Start
+sudo mkdir -p /var/spool/pbs/datastore
 sudo chown -R postgres:postgres /var/spool/pbs/datastore
 
-PBS_DATA_SERVICE_USER=postgres; sudo systemctl start pbs
+PBS_DATA_SERVICE_USER=postgres; sudo /etc/init.d/pbs start
