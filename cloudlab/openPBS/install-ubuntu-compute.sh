354ddc84b43eac2aaea4c1c22ae6135a248de034
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-26 22:35:07 -0600

    11th attemp at openPBS server

--- cloudlab/openPBS/install-ubuntu-compute.sh
+++ cloudlab/openPBS/install-ubuntu-compute.sh
@@ -2,7 +2,7 @@
 
 # Install openPBS on Ubuntu 20.04
 sudo apt update
-sudo apt-get -y expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
+sudo apt-get -y install expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
       sudo tcl tk libical3 postgresql-server-dev-all
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb
--- cloudlab/openPBS/install-ubuntu-server.sh
+++ cloudlab/openPBS/install-ubuntu-server.sh
@@ -2,16 +2,18 @@
 
 # Install openPBS on Ubuntu 20.04
 sudo apt update
-sudo apt-get -y expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
+sudo apt-get -y install expat libedit2 postgresql python3 postgresql-contrib sendmail-bin \
       sudo tcl tk libical3 postgresql-server-dev-all
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
 
-# Start postgresql service
-sudo systemctl start postgresql
 
 # Start
 sudo mkdir -p /var/spool/pbs/datastore
 sudo chown -R postgres:postgres /var/spool/pbs/datastore
+
+# Start postgresql service
+# sudo systemctl start postgresql
+pg_ctlcluster 12 main start
 
 PBS_DATA_SERVICE_USER=postgres; sudo /etc/init.d/pbs start
