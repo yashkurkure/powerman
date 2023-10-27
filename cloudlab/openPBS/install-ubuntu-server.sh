95b7e3006142c4d49f42090222b74b5ec46c43bc
yashkurkure <yash.kurkure1903@gmail.com>
2023-10-27 10:49:12 -0600

    separate installation and configuration

--- cloudlab/openPBS/install-ubuntu-server.sh
+++ cloudlab/openPBS/install-ubuntu-server.sh
@@ -6,14 +6,3 @@ sudo apt-get -y install expat libedit2 postgresql python3 postgresql-contrib sen
       sudo tcl tk libical3 postgresql-server-dev-all
 
 sudo apt-get -y install /local/repository/cloudlab/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
-
-
-# Start
-sudo mkdir -p /var/spool/pbs/datastore
-sudo chown -R postgres:postgres /var/spool/pbs/datastore
-
-# Start postgresql service
-# sudo systemctl start postgresql
-pg_ctlcluster 12 main start
-
-PBS_DATA_SERVICE_USER=postgres; sudo /etc/init.d/pbs start
---
+++ cloudlab/openPBS/setup_compute.sh
@@ -0,0 +1,11 @@
+sudo echo "PBS_EXEC=/opt/pbs
+PBS_SERVER=head
+PBS_START_SERVER=0
+PBS_START_SCHED=0
+PBS_START_COMM=0
+PBS_START_MOM=1
+PBS_HOME=/var/spool/pbs
+PBS_CORE_LIMIT=unlimited
+PBS_SCP=/usr/bin/scp" > /etc/pbs.conf
+
+sudo systemctl start pbs
