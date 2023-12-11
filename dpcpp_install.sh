cb95994d388766fcc954ac8db1778f2d3e8fe7b1
yashkurkure <yash.kurkure1903@gmail.com>
2023-12-11 16:22:21 -0600

    cmake 3.27 install script

--- dpcpp_install.sh
+++ dpcpp_install.sh
@@ -2,6 +2,9 @@ export DPCPP_HOME=~/sycl_workspace
 mkdir $DPCPP_HOME
 cd $DPCPP_HOME
 
+mkdir -p cmake-3.27 && wget -qO- "https://cmake.org/files/v3.27/cmake-3.27.0-linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C cmake-3.27
+~ $ export PATH=`pwd`/cmake-3.27/bin:$PATH
+
 git clone https://github.com/intel/llvm -b sycl
 
 python3 $DPCPP_HOME/llvm/buildbot/configure.py
