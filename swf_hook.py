1178292f0ba90ac67802a25c6d9b8f5b04f1decc
yashkurkure <yash.kurkure1903@gmail.com>
2024-01-22 15:15:39 -0600

    fixs and comments

--- swf_hook.py
+++ swf_hook.py
@@ -2,6 +2,8 @@ import pbs
 import sys
 import os
 '''
+Contents of SWF row:
+
 1. Job Number -- a counter field, starting from 1.
 2. Submit Time -- in seconds. The earliest time the log refers to is zero, and is usually the submittal time of the first job. The lines in the log are sorted by ascending submittal times. It makes sense for jobs to also be numbered in this order.
 3. Wait Time -- in seconds. The difference between the job's submit time and the time at which it actually began to run. Naturally, this is only relevant to real logs, not to models.
@@ -22,6 +24,32 @@ Note that when values are rounded to an integral number of seconds (as often hap
 16. Partition Number -- a natural number, between one and the number of different partitions in the systems. The nature of the system's partitions should be explained in a header comment. For example, it is possible to use partition numbers to identify which machine in a cluster was used.
 17. Preceding Job Number -- this is the number of a previous job in the workload, such that the current job can only start after the termination of this preceding job. Together with the next field, this allows the workload to include feedback as described below.
 18. Think Time from Preceding Job -- this is the number of seconds that should elapse between the termination of the preceding job and the submittal of this one.
+
+
+Example:
+0 1    2     3    4   5  6   7  8     9   10  11  12   13 14 15 16 17
+1 0000 11464 4000 40  -1 -1  40 4000  -1  1   17  17   -1 -1 -1 -1 -1
+
+CQSim:
+'id':int(temp_dataList[0]),\
+'submit':self.density*(float(temp_dataList[1])-min_sub)+temp_start,\
+'wait':float(temp_dataList[2]),\
+'run':float(temp_dataList[3]),\
+'usedProc':int(temp_dataList[4]),\
+'usedAveCPU':float(temp_dataList[5]),\
+'usedMem':float(temp_dataList[6]),\
+'reqProc':int(temp_dataList[7]),\
+'reqTime':float(temp_dataList[8]),\
+'reqMem':float(temp_dataList[9]),\
+'status':int(temp_dataList[10]),\
+'userID':int(temp_dataList[11]),\
+'groupID':int(temp_dataList[12]),\
+'num_exe':int(temp_dataList[13]),\
+'num_queue':int(temp_dataList[14]),\
+'num_part':int(temp_dataList[15]),\
+'num_pre':int(temp_dataList[16]),\
+'thinkTime':int(temp_dataList[17]),\
+
 '''
 
 e = pbs.event()
