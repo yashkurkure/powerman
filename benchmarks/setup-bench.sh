4fc3d418610ea78e1a62b057ea82de1fa4be290a
yashkurkure <yash.kurkure1903@gmail.com>
2024-02-13 14:44:50 -0600

    compiling npb

--- benchmarks/setup-bench.sh
+++ benchmarks/setup-bench.sh
@@ -4,6 +4,39 @@
 
 # Extract the files for the benchmark into NFS directory
 tar -xvf /local/repository/benchmarks/NPB3.4.2.tar -C /pbsusers
-mv /pbsusers/NPB3.4.2 /pbsusers/npb
 
-# Load default configurations for MPI and OMP
+# Extracted to: /pbsusers/NPB3.4.2
+
+# Build MPI benchmarks
+cp /pbsusers/NPB3.4.2/NPB3.4-MPI/config/make.def.template /pbsusers/NPB3.4.2/NPB3.4-MPI/config/make.def
+
+echo "ft	S
+mg	S
+sp	S
+lu	S
+bt	S
+is	S
+ep	S
+cg	S
+dt	S
+ft	W
+mg	W
+sp	W
+lu	W
+bt	W
+is	W
+ep	W
+cg	W
+dt	W
+ft	A
+mg	A
+sp	A
+lu	A
+bt	A
+is	A
+ep	A
+cg	A
+dt	A" > /pbsusers/NPB3.4.2/NPB3.4-MPI/config/suite.def
+
+cd /pbsusers/NPB3.4.2/NPB3.4-MPI/ && make suite
+
