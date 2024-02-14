606e20772c5fad2e07d1cc0bf54d00fee19e06d3
yashkurkure <yash.kurkure1903@gmail.com>
2024-02-14 16:07:57 -0600

    formatting

--- benchmarks/jobtracegen.py
+++ benchmarks/jobtracegen.py
@@ -1,5 +1,5 @@
 from jinja2 import Template
-'''
+"""
 This script generates job traces based on the following paper with a 
 few modifications:
 
@@ -8,21 +8,6 @@ N. Kumbhare, A. Marathe, A. Akoglu, H. J. Siegel, G. Abdulla and S. Hariri,
 Oversubscribed HPC Systems," in IEEE Transactions on Parallel and Distributed 
 Systems, vol. 31, no. 6, pp. 1419-1433, 1 June 2020, 
 doi: 10.1109/TPDS.2020.2967373.
-
-The script creates 15 unique workload traces. Each trace is composed of 60 jobs 
-in the order of their arrival time. In all the traces, we use an inter-arrival 
-duration of 200 seconds between consecutive job. 
-
-It randomly select the range for possible node configurations for each job while
-ensuring that its maximum node count does not exceed the system size. The paper
-randomly chooses the range of OpenMP thread count in between 8 and 24.
-
-For the walltime the paper estimates the job execution time on an intermediate 
-node count (geometric mean of the maximum and minimum nodes requested by the 
-job).(TODO: what does this mean??)
-
-TODO: what walltime to use for each job? for now randomly select between 0.5, 1
-and 1.5 hrs. Some jobs might fail to complete in the specified time due to this.
 
 The job workloads are Multi-zone versions of NPB (NPB-MZ) that are designed to 
 exploit multiple levels of parallelism in applications and to test the 
@@ -56,14 +41,59 @@ Class   Number of Zones     Aggregate Grid    Memory Requirement
  E      64 x 64    4 x 4    4224 x 3456 x 92        250 GB
  F     128 x 128   4 x 4   12032 x 8960 x 250       5.0 TB
 -----------------------------------------------------------------
+"""
+
+"""
+Creates 15 unique workload traces. Each trace is composed of 60 jobs 
+in the order of their arrival time. In all the traces, the inter-arrival 
+duration of 200 seconds between consecutive job. 
+"""
+num_traces = 15
+jobs_per_trace = 60
+arrival_delta = 200
+
+"""
+Randomly select the range for possible node configurations for each job while
+ensuring that its maximum node count does not exceed the system size.
+"""
+node_counts = [i for i in range(1, 25)]
+
+"""
+Randomly chooses the range of OpenMP thread count in between 8 and 24
+"""
+omp_thread_counts = [i for i in range(8, 25)]
+
+"""
+The job workloads are Multi-zone versions of NPB (NPB-MZ) that are designed to 
+exploit multiple levels of parallelism in applications and to test the 
+effectiveness of multi-level and hybrid parallelization paradigms and tools. 
+
+Randomly select the problem type and class.
 
 The paper uses a sampling range for the problem size between class C and class 
 E with a memory footprint between 0.8 and 250 GB for the above problems. This
 script does the same but between class S and class D.
-'''
 
+TODO: Test class E and F
+"""
+problems = ['bt-mz','lu-mz','sp-mz']
+problem_classes = ['S', 'W', 'A', 'B', 'C', 'D']
 
+"""
+For the walltime the paper estimates the job execution time on an intermediate 
+node count (geometric mean of the maximum and minimum nodes requested by the 
+job).(TODO: what does this mean??)
 
+TODO: what walltime to use for each job? for now randomly select between 0.5, 1
+and 1.5 hrs. Some jobs might fail to complete in the specified time due to this.
+"""
+walltimes = ['00:30:00', '01:00:00', '01:30:00']
+
+"""
+Set the path for the location of the benchmark executables.
+"""
+benchmark_path = '/pbsusers/NPB3.4.2/NPB3.4-MPI/bin/'
+ 
 job_template = """
 #!/bin/bash
 #PBS -l nodes={{ nodes }}:ppn={{ ppn }}
@@ -77,15 +107,6 @@ export P4_RSHCOMMAND=/opt/pbs/bin/pbs_tmrsh
 export OMP_NUM_THREADS={{ OMP_NUM_THREADS }}
 
 mpirun {{executable path}}
-
 """
 
-arrival_delta = 200
-num_traces = 15
-jobs_per_trace = 60
-omp_thread_counts = [ i for i in range(8, 25) ]
-node_counts = [i for i in range(1, 25) ]
-walltimes = ['00:30:00', '01:00:00', '01:30:00']
-problem_classes = ['S', 'W', 'A', 'B', 'C', 'D']
-benchmark_path = '/pbsusers/NPB3.4.2/NPB3.4-MPI/bin/'
-problems = ['bt-mz','lu-mz','sp-mz']
+
