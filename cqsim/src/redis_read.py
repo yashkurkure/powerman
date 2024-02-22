588958fe0eb6705db62eb22e9fffccc6d73322c2
yashkurkure <yash.kurkure1903@gmail.com>
2024-02-22 16:37:57 -0600

    todos for cqsim integ

--- cqsim/src/redis_read.py
+++ cqsim/src/redis_read.py
@@ -2,26 +2,16 @@ import redis
 import json
 import os
 
-swf_lines = []
 qstat = {}
 location = '/pbsusers/log.swf'
 
 def process_stream_entry(data):
-    # TODO: Process to SWF formats
     redis_id = data[0]
     timestamp_ms = int(redis_id.split('-')[0])
     timestamp_s = int(timestamp_ms/1000)
-    # job_id = data[1]['job_id']
     event_type = data[1]['event_type']
-    # event_code = data[1]['event_code']
     json_data = json.loads(data[1]['json_data'])
     id = int(data[1]['job_id'].split('.')[1])
-
-    """
-    queuejob -> 0, 1*, 7*, 8*, 9, 11, 12, 13, 14, 15, 
-    runjob,execjob_begin -> 2, 3
-    execjob_end ->  4, 5, 6, 10, 16?, 17?
-    """
     if event_type == 'q':
         # Parameters to record
         submit = timestamp_s
@@ -72,93 +62,30 @@ def parse_args():
     Parse the args.
     """
     import argparse
-    # print("----Args----")
     parser = argparse.ArgumentParser(description="Argument Parser")
 
     parser.add_argument("-s", "--redis_stream", type=str, default="redis-hook",
                         help="Redis stream name (default: 15)")
 
     args = parser.parse_args()
-    # print("Redis stream:", args.redis_stream)
-    # print("----Args----")
     return args
 
 def write_swf_json(j):
     print(json.dumps(j))
 
+def get_swf(allowPartial = False):
+    '''
+    from the queue of data reported by the hook, create 
+   input for cqsim.
+    '''
+    pass
 
-def create_swf_entry(
-        job_num,     # 0 
-        submit_time,    # 1
-        wait_time,      # 2
-        run_time,       # 3
-        num_proc,       # 4
-        avg_cpu_time,   # 5
-        mem_usage,      # 6
-        req_num_p,      # 7 same as # 4
-        req_time,       # 8
-        req_mem,        # 9
-        job_status,     # 10
-        user_id,        # 11
-        group_id,       # 12
-        executable,     # 13
-        queue_num,      # 14
-        partition_num,  # 15
-        preceding_job_num,  #16
-        think_time      #17
-):
-    return [
-        job_num,     # 0 
-        submit_time,    # 1
-        wait_time,      # 2
-        run_time,       # 3
-        num_proc,       # 4
-        avg_cpu_time,   # 5
-        mem_usage,      # 6
-        req_num_p,      # 7 same as # 4
-        req_time,       # 8
-        req_mem,        # 9
-        job_status,     # 10
-        user_id,        # 11
-        group_id,       # 12
-        executable,     # 13
-        queue_num,      # 14
-        partition_num,  # 15
-        preceding_job_num,  #16
-        think_time      #17
-    ]
+def run_cq_sim(data):
+    '''
+    run cqsim given data
+    '''
+    pass
 
-def create_swf_json(
-        job_num,     # 0 
-        submit_time,    # 1
-        wait_time,      # 2
-        run_time,       # 3
-        num_proc,       # 4
-        avg_cpu_time,   # 5
-        mem_usage,      # 6
-        req_num_p,      # 7 same as # 4
-        req_time,       # 8
-        req_mem,        # 9
-        job_status,     # 10
-        user_id,        # 11
-        group_id,       # 12
-        executable,     # 13
-        queue_num,      # 14
-        partition_num,  # 15
-        preceding_job_num,  #16
-        think_time      #17
-):
-    return {
-	'id':job_num,
-	'submit': submit_time,
-    'reqProc': num_proc,
-	'reqTime': req_time,
-	'reqMem': req_mem,
-	'wait': wait_time,
-	'run': run_time,
-	'usedProc': num_proc,
-	'status': job_status,
-	}
 
 if __name__ == "__main__":
     args = parse_args()
@@ -173,7 +100,8 @@ if __name__ == "__main__":
 
         # Wait for new entry
         latest = r.xread({stream_name: last_id}, None, 0)[0]
-        # print(f'** New In Stream **')
+
+        # Parse stream entry
         stream_name = latest[0]
         stream_data = latest[1]
         for data in stream_data:
@@ -181,9 +109,8 @@ if __name__ == "__main__":
             job_id = data[1]['job_id']
             event_type = data[1]['event_type']
             event_code = data[1]['event_code']
-            # print(f'{redis_id}\t{job_id}\t{event_type}\t{event_code}')
             process_stream_entry(data)
             last_id = redis_id
-        # print(f'*******************')
 
-# 1 1641021254 52645 10849 128 -1 -1 128 10800 -1 0 -1 -1 -1 -1 -1 -1 0
+    # TODO: When the program terminates dump the info
+    # collected as a SWF file.
