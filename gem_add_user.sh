84140b0acb12b6f176809d7a0d8772d2c9fd0d14
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-03 17:55:05 -0600

    gemini ldap

---
+++ gem_add_user.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 1. Get user details
+read -p "Enter username: " username
+read -sp "Enter password: " password
+
+# 2. Hash password (SSHA method recommended)
+hashed_password=$(slappasswd -s "$password")
+
+# 3. Create an LDIF file for the user
+cat << EOF > user.ldif
+dn: uid=$username,ou=users,dc=example,dc=com
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+cn: $username 
+sn: $username 
+uid: $username
+uidNumber: $(expr $(id -u) + 1000)  # Find next available UID
+gidNumber: 1000  # Or any primary group ID
+homeDirectory: /home/$username
+userPassword: $hashed_password
+
+EOF
+
+# 4. Add the user to LDAP
+sudo ldapadd -x -D cn=admin,dc=example,dc=com -W -f user.ldif 
+
+# 5. Create the user on the system
+sudo useradd -m -d /home/$username -s /bin/bash $username
+
+# 6. Set the user's password
+echo -e "$password\n$password" | sudo passwd $username
---
+++ gem_ldap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 1. Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+
+# 2. Install OpenLDAP packages
+sudo apt install slapd ldap-utils -y
+
+# 3. Reconfigure slapd (OpenLDAP server)
+sudo dpkg-reconfigure slapd
+
+# During reconfiguration, provide the following:
+# * Omit OpenLDAP server configuration?  -> No
+# * DNS domain name -> Your domain name (e.g., example.com)
+# * Organization name -> Your organization's name 
+# * Administrator password -> A secure password for LDAP administration
+# * Database backend to use? -> MDB
+# * Remove the database when slapd is purged? -> No
+# * Move old database? -> Yes
+# * Allow LDAPv2 protocol? -> No
+
+# 4. Create a base LDAP structure (adjust domain components as needed)
+sudo ldapadd -x -D cn=admin,dc=example,dc=com -W -f base.ldif
+
+# Where 'base.ldif' contains:
+# dn: dc=example,dc=com
+# objectClass: dcObject
+# objectClass: organization
+# dc: example
+# o: Example Organization
+
+# 5. Add organizational units for 'users' and 'groups'
+sudo ldapadd -x -D cn=admin,dc=example,dc=com -W -f ou.ldif 
+
+# Where 'ou.ldif' contains:
+# dn: ou=users,dc=example,dc=com
+# objectClass: organizationalUnit
+# ou: users
+
+# dn: ou=groups,dc=example,dc=com
+# objectClass: organizationalUnit
+# ou: groups 
