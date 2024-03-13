ee8f28e71184766887b33ecbe47db523b24d4840
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-13 17:45:09 -0500

    ansible var load

--- bird.py
+++ bird.py
@@ -3,6 +3,7 @@ import time
 import ansible.inventory.manager
 from ansible.parsing.dataloader import DataLoader
 from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.vars.manager import VariableManager 
 
 # Configuration (modify as needed)
 INVENTORY_PATH = '/local/auto.inventory'
@@ -12,6 +13,7 @@ CHECK_INTERVAL = 5  # Seconds
 # Load Ansible inventory
 loader = DataLoader()
 inventory = ansible.inventory.manager.InventoryManager(loader=loader, sources=[INVENTORY_PATH])
+variable_manager = VariableManager(loader=loader, inventory=inventory)
 worker_hosts = inventory.get_groups_dict()['workernodes']
 print(worker_hosts)
 
@@ -30,7 +32,13 @@ playbook = [
 ]
 
 # Run the playbook using Ansible Playbook Executor
-pbex = PlaybookExecutor(playbooks=playbook, inventory=inventory)
+pbex = PlaybookExecutor(
+    playbooks=playbook, 
+    inventory=inventory, 
+    variable_manager=variable_manager,
+    loader=loader,
+    passwords={}
+)
 results = pbex.run()
 
 # Analyze results
