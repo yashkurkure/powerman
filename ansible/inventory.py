3eeb44cb33a3cf1092f074b158ac8eebb97add17
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-16 02:50:03 -0500

    replace tab; remove jinja

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -2,56 +2,31 @@
 This script generates an Ansible Inventory file for a cluster on cloudlab.
 """
 import argparse
-from jinja2 import Template
-
-job_template = """
-headnodes:
-  hosts:
-    localhost:
-workernodes:
-  hosts:
-    node0.testbed.schedulingpower.emulab.net:
-    node1.testbed.schedulingpower.emulab.net:
-    node2.testbed.schedulingpower.emulab.net:
-loginnodes:
-  hosts:
-    login.testbed.schedulingpower.emulab.net:
-all:
-  vars:
-    ansible_user: root
-    ansible_private_key_file: /root/.ssh/id_rsa
-    ansible_host_key_checking: False
-    numworkernodes: 3
-    username: ykurkure
-    servercanonicalname: pc557.emulab.net
-    serverhostname: head.testbed.schedulingpower.emulab.net
-"""
-template = Template(job_template)
 
 def generate(args):
     import socket
     hostname = str(socket.gethostname())
     with open(args.gen_path, 'w+') as f:
         f.write(f'headnode:\n')
-        f.write(f'\thosts:\n')
-        f.write(f'\t\tlocalhost:\n')
+        f.write(f'  hosts:\n')
+        f.write(f'    localhost:\n')
         f.write(f'workernodes:\n')
-        f.write(f'\thosts:\n')
+        f.write(f'  hosts:\n')
         for i in range(0, args.number_of_worker_nodes):
             node_name = hostname.replace('head',f'node{i}')
-            f.write(f'\t\t{node_name}:\n')
+            f.write(f'    {node_name}:\n')
         f.write(f'loginnodes:\n')
-        f.write(f'\thosts:\n')
+        f.write(f'  hosts:\n')
         for i in range(0, args.number_of_login_nodes):
             node_name = hostname.replace('head',f'login{i}')
-            f.write(f'\t\t{node_name}:\n')
+            f.write(f'    {node_name}:\n')
         f.write(f'all:\n')
-        f.write(f'\tvars:\n')
-        f.write(f'\t\tansible_user: root\n')
-        f.write(f'\t\tansible_private_key_file: /root/.ssh/id_rsa\n')
-        f.write(f'\t\tansible_host_key_checking: False\n')
-        f.write(f'\t\tnumworkernodes: {args.number_of_worker_nodes}\n')
-        f.write(f'\t\tserverhostname: {hostname}\n')
+        f.write(f'  vars:\n')
+        f.write(f'    ansible_user: root\n')
+        f.write(f'    ansible_private_key_file: /root/.ssh/id_rsa\n')
+        f.write(f'    ansible_host_key_checking: False\n')
+        f.write(f'    numworkernodes: {args.number_of_worker_nodes}\n')
+        f.write(f'    serverhostname: {hostname}\n')
     pass
 
 
