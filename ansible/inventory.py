d200a2418be4f6241dbc853e87e25d018a866184
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-16 02:09:27 -0500

    inventory gen

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -2,7 +2,55 @@
 This script generates an Ansible Inventory file for a cluster on cloudlab.
 """
 import argparse
+from jinja2 import Template
 
+job_template = """
+headnodes:
+  hosts:
+    localhost:
+workernodes:
+  hosts:
+    node0.testbed.schedulingpower.emulab.net:
+    node1.testbed.schedulingpower.emulab.net:
+    node2.testbed.schedulingpower.emulab.net:
+loginnodes:
+  hosts:
+    login.testbed.schedulingpower.emulab.net:
+all:
+  vars:
+    ansible_user: root
+    ansible_private_key_file: /root/.ssh/id_rsa
+    ansible_host_key_checking: False
+    numworkernodes: 3
+    username: ykurkure
+    servercanonicalname: pc557.emulab.net
+    serverhostname: head.testbed.schedulingpower.emulab.net
+"""
+template = Template(job_template)
+
+def generate(args):
+    import socket
+    hostname = str(socket.gethostname())
+
+    print(f'headnode:')
+    print(f'\thosts:')
+    print(f'\t\tlocalhost:')
+    print(f'workernodes:')
+    print(f'\thosts:')
+    for i in range(0, args.number_of_worker_nodes):
+        print(f'\t\t{hostname.replace('head',f'node{i}')}:')
+    print(f'loginnodes:')
+    for i in range(0, args.number_of_worker_nodes):
+        print(f'\t\t{hostname.replace('head',f'login{i}')}:')
+    print(f'all:')
+    print(f'\tvars:')
+    print(f'\t\tansible_user: root')
+    print(f'\t\tansible_private_key_file: /root/.ssh/id_rsa')
+    print(f'\t\tansible_host_key_checking: False')
+    print(f'\t\tnumworkernodes: {args.number_of_worker_nodes}')
+    print(f'\t\tserverhostname: {hostname}')
+
+    pass
 
 
 def parse_args():
@@ -28,3 +76,4 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
+    generate(args)
