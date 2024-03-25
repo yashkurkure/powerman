cd19f681fb60aa261712b94448887b34de249d86
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-25 18:30:12 -0400

    pass paths to packages

--- cloudlab/setup.sh
+++ cloudlab/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # NOTE: Must be executed as root
 
-
 ansible-playbook \
     -i /local/cluster_inventory.yml\
     /local/repository/ansible/nfs.yml
 
 ansible-playbook \
     -i /local/cluster_inventory.yml\
+    --extra-vars "\
+    headnodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb \
+    workernodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb \
+    loginnodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb"\
     /local/repository/ansible/openpbs.yml
 
 ansible-playbook \
