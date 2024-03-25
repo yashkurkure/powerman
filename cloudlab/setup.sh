601df2c2239bb66693ab5238bbca1ed64fb69e77
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-25 18:45:43 -0400

    udpate openpbs install script

--- cloudlab/setup.sh
+++ cloudlab/setup.sh
@@ -5,12 +5,12 @@ ansible-playbook \
     -i /local/cluster_inventory.yml\
     /local/repository/ansible/nfs.yml
 
+path_server=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
+path_mom=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb
+path_client=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb
 ansible-playbook \
-    -i /local/cluster_inventory.yml\
-    --extra-vars "\
-    headnodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb \
-    workernodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb \
-    loginnodes_deb=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb"\
+    -i /local/cluster_inventory.yml \
+    --extra-vars "headnode_deb=$path_server workernodes_deb=$path_mom loginnodes_deb=$path_client"\
     /local/repository/ansible/openpbs.yml
 
 ansible-playbook \
--- cloudlab/temp.sh
+++
@@ -1,8 +0,0 @@
-path_server=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_server.deb
-path_mom=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_execution.deb
-path_client=/local/repository/packages/openPBS/openpbs_23.06.06.ubuntu_20.04/openpbs_client.deb
-
-ansible-playbook \
-    -i /local/cluster_inventory.yml \
-    --extra-vars "headnode_deb=$path_server workernodes_deb=$path_mom loginnodes_deb=$path_client"\
-    /local/repository/ansible/openpbs.yml
