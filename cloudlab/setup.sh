9639cd71927e72c33c11f3a99e0952b294e6603b
yashkurkure <yash.kurkure1903@gmail.com>
2024-03-25 15:08:23 -0400

    mpich & ssh setup

--- cloudlab/setup.sh
+++ cloudlab/setup.sh
@@ -9,3 +9,12 @@ ansible-playbook \
 ansible-playbook \
     -i /local/cluster_inventory.yml\
     /local/repository/ansible/openpbs.yml
+
+ansible-playbook \
+    -i /local/cluster_inventory.yml\
+    /local/repository/ansible/mpich.yml
+
+ansible-playbook \
+    -i /local/cluster_inventory.yml\
+    --extra-vars "username=ykurkure groupname=SchedulingPower"\
+    /local/repository/ansible/mpich.yml
