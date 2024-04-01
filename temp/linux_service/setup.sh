f836f1d2997caf68ad9ef744d927b5d7fcf86bce
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-01 17:42:44 -0500

    change service location

--- temp/linux_service/setup.sh
+++ temp/linux_service/setup.sh
@@ -5,3 +5,4 @@
 sudo cp /local/repository/temp/linux_service/myservice.service /etc/systemd/system/myservice.service
 sudo systemctl enable myservice.service
 sudo systemctl start myservice.service
+touch /pbsusers/myservice.log
