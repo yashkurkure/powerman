e703fd5cb21d8d64ef339cfc8b922ef708fe5a81
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-01 17:15:58 -0500

    exec perm

--- temp/linux_service/setup.sh
+++ temp/linux_service/setup.sh
@@ -1,3 +1,7 @@
+# Notes
+# View logs: journalctl -u myservice.service
+
+
 sudo cp /local/repository/temp/linux_service/myservice.service /etc/systemd/system/myservice.service
 sudo systemctl enable myservice.service
 sudo systemctl start myservice.service
