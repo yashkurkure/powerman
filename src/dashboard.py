10677501d0b3136a95c841e581ddacd23535c113
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-07 15:58:41 -0500

    dash expc

--- src/dashboard.py
+++ src/dashboard.py
@@ -8,10 +8,32 @@ script_path = "/pbsusers/trace_0/submit_trace.sh"
 
 app = dash.Dash(__name__)
 
+
 app.layout = html.Div([
-    html.Button('Run Script', id='run-script-button', style={'disabled': False}), 
-    html.Div(id='output-div'),
-    dcc.Interval(id='interval', interval=1000, n_intervals=0)
+    dcc.Tabs([
+        dcc.Tab(label='Welcome', children=[
+            html.Button('Run Script', id='run-script-button', style={'disabled': False}), 
+            html.Div(id='output-div'),
+            dcc.Interval(id='interval', interval=1000, n_intervals=0)
+        ]),
+        dcc.Tab(label='Qstat Output', children=[
+            # Assumes your web server serves the qstat output at '/qstat-output'
+        ])
+    ])
+])
+
+app.layout = html.Div([
+    dcc.Tabs(id="tabs", value='tabs', children=[
+        dcc.Tab(label='Welcome', value='tab-1', children=[
+            html.H1("Welcome to the Dashboard"),
+            html.Button('Run Script', id='run-script-button', style={'disabled': False}), 
+            html.Div(id='output-div'),
+            dcc.Interval(id='interval', interval=1000, n_intervals=0)
+        ]),
+        dcc.Tab(label='Events', value='tab-2', children=[
+            html.Div(id='event-output')
+        ])
+    ])
 ])
 
 @app.callback(
@@ -40,7 +62,27 @@ def trigger_script_and_check(n_clicks, n_intervals, button_disabled):
                 result_message += f'\nErrors:\n{errors.decode()}'
             return False, result_message, 0  # Re-enable button 
 
-    return button_disabled, 'Script is running...', n_intervals + 1 
+    return button_disabled, 'Script is running...', n_intervals + 1
+
+# Simulate a blocking call (modify this for your actual task)
+def blocking_task():
+    from time import sleep
+    for i in range(5):
+        sleep(2)  # Simulate some work
+        return f"Event {i + 1} occurred"
+
+# Callback to update the 'Events' tab
+@app.callback(
+    output=dash.dependencies.Output('event-output', 'children'),
+    inputs=[dash.dependencies.Input('tabs', 'value')],
+)
+def update_events(tab):
+    if tab == 'tab-2':
+        events = []
+        while True:
+            new_event = blocking_task()
+            events.append(html.P(new_event))  
+            return events 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
