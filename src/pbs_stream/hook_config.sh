eebc777fe1ec784d4997593cbfbf677d5c85107d
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-07 22:23:33 -0500

    exec perm

--- src/pbs_stream/hook_config.sh
+++ src/pbs_stream/hook_config.sh
@@ -1,4 +1,4 @@
 /opt/pbs/bin/qmgr -c 'create hook redis_hook'
 /opt/pbs/bin/qmgr -c 'import hook redis_hook application/x-python default ./redis_hook.py'
-/opt/pbs/bin/qmgr -c 'set hook redis_hook event = "queuejob,runjob,execjob_end"'
+/opt/pbs/bin/qmgr -c 'set hook redis_hook event = "queuejob,runjob,execjob_run,execjob_end"'
 /opt/pbs/bin/qmgr -c 'set hook redis_hook debug = True'
