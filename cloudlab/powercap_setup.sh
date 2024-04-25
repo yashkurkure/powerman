a52c1bdc8c8cc87fd76ca63436371347039b5c93
yashkurkure <yash.kurkure1903@gmail.com>
2024-04-25 04:20:57 -0500

    powercap from ansible

--- cloudlab/powercap_setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y python3-pip
-pip3 install matplotlib
-sudo apt install -y cmake stress
-sudo modprobe intel_rapl_msr
-cd /local && git clone https://github.com/powercap/powercap.git
-cd /local/powercap && mkdir _build && cd _build && cmake .. && make && sudo make install
